09f5d4cbfdfe0a2eea8f248d4cd5a559a7c5352a
DeniD-star <dimitrova.denise@gmail.com>
2023-07-21 10:09:19 +0200

    dipendency injection principle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,4 +21,81 @@ users: User[]= [
     console.log('Event');
 
   }
+
+  constructor(){
+    //po tozi na4in, prosto s pushvane , ne se trigurva changeDetectora
+    setInterval(()=>{
+      this.users.push({
+        name: 'DemoName',
+        age: 0
+      })
+//ako e po tozi na4in 4rez pushvane, i ne 4rez smqna na referenciqta, prosto trqbva
+// da izvikam funkziqta refresh, za da mi vidi promenite.
+      console.log('User has been added!');
+
+    }, 3000)
+  }
+
+  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement){
+    const user = {
+        name: inputName.value,
+        age: Number(inputAge.value)
+    }
+    this.users.push(user)
+    /*kak moje naro4no da trigurnem changeDetectora? , mojem vmesto da pushnem usera, da promenim referenziqta i da
+    napravim nova 4rez assign-vane , taka change detectora se aktivira, poneje otnovo imame promqna na kolekciq
+    this.users = [...this.users, user]*/
+
+    inputName.value = "";
+    inputAge.value = "";
+  }
 }
+
+class Wallet{
+
+  balance  = 2000;
+
+  constructor(balance: number){
+    this.balance = balance;
+  }
+}
+class Person{
+
+  name  = 'Ivan';
+
+  constructor(name: string){
+    this.name = name;
+  }
+}
+
+class Car{
+  carModel = 'BMW';
+
+  constructor(carModel: string){
+    this.carModel = carModel;
+
+
+  }
+}
+
+class BetterPersonWithNameBalanceAndCar{
+  person: Person;
+  car: Car;
+  wallet: Wallet;
+
+  constructor(person: Person, car:Car, wallet: Wallet){
+    this.person = person;
+    this.car = car;
+    this.wallet = wallet;
+
+  }
+}
+
+const personOne = new Person('Stefan');
+const carOne = new Car('Audi');
+const walletOne = new Wallet(2500)
+
+const betterPerson1 = new BetterPersonWithNameBalanceAndCar(personOne, carOne, walletOne);//that's called dependency injection,
+//when we works with abstractions(podavame si celi abstrakcii(classove, ne prosto properties))
+
+
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,13 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
 import { User } from '../type/User';
+
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush //vse edno kazvame ti nqma da se renderira6 pove4e dokato ne ti kaja
 })
+
+/*change detection strategiqta se trigurva pri promqna na danni ili kolekciq ot danni, ako nqmame pribaven onPush,
+ozna4ava 4e rabotim s defaultnata changeDetection strategy */
 export class UserListComponent {
 @Input() users: User[]= []
 
- 
+constructor(private cd: ChangeDetectorRef){//ozna4ava 4e go o4akvame kato instanziq changeDetectora da ni se podade
+
+}
+ngOnChanges(){
+  console.log("Invocked from ngOnChanges!");
+//tova se trigurva i hva6ta vsi4ki changiove,toest se trigurva kogato se trigurva i changeDetectionStrategy
+}
+
+refresh(){// tazi funkziq ni izvikva ot changeDetectora detectChanges
+  this.cd.detectChanges
+}
 }
